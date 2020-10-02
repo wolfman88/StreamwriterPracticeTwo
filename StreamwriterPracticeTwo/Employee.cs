ff0ec2f40fb61f6b06398746a903c02fdc046be8
Robert Baskin <rbaskin@gmail.com>
2020-10-02 08:12:56 -0400

    Add project files.

---
+++ StreamwriterPracticeTwo/Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamwriterPracticeTwo
+{
+  class Employee
+  {
+    public Employee()
+    {
+
+    }
+
+    public string ID { get; set; } // 0
+    public string NamePrefix { get; set; } // 1
+    public string FirstName { get; set; } // 2
+    public string MiddleInitial { get; set; } // 3
+    public string LastName { get; set; } // 4
+    public string Gender { get; set; } // 5
+    public DateTime DrugTestDateLast { get; set; } // 6
+    public string EMail { get; set; } // 7
+    public DateTime DateOfBirth { get; set; } // 8
+    public DateTime DateHired { get; set; } // 9
+    public decimal Salary { get; set; } // 10
+    public decimal LastPayHike { get; set; } // 11
+    public string SSN { get; set; } // 12
+    public string PhoneNumber { get; set; } // 13
+    public string County { get; set; } // 14
+    public string City { get; set; } // 15 
+    public string State { get; set; } // 16
+    public string ZipCode { get; set; } // 17
+    public string UserName { get; set; } // 18
+    public string Password { get; set; } // 19
+
+    public Employee GetEmployee(string ID)
+    {
+      Employee emp = new Employee();
+      return emp;
+    }
+  }
+}
---
+++ StreamwriterPracticeTwo/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace StreamwriterPracticeTwo
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      string line = "";
+
+      StreamReader reader = new StreamReader(@"EmployeeRecords.csv");
+      Dictionary<string, Employee> employeeInfoData = new Dictionary<string, Employee>();
+
+      int i = 1;
+
+      while (line != null)
+      {
+        line = reader.ReadLine();
+
+        if (line != null)
+        {
+          if (i > 1)
+          {
+            string[] employeeInformation = line.Split(',');
+            // got stuck here
+            Employee emp = new Employee();
+            emp.ID = employeeInformation[0];
+            emp.NamePrefix = employeeInformation[1];
+            emp.FirstName = employeeInformation[2];
+            emp.MiddleInitial = employeeInformation[3];
+            emp.LastName = employeeInformation[4];
+            emp.Gender = employeeInformation[5];
+            emp.DrugTestDateLast = Convert.ToDateTime(employeeInformation[6]);
+            emp.EMail = employeeInformation[7];
+            emp.DateOfBirth = Convert.ToDateTime(employeeInformation[8]);
+            emp.DateHired = Convert.ToDateTime(employeeInformation[9]);
+            emp.Salary = Convert.ToDecimal(employeeInformation[10]);
+            emp.LastPayHike = Convert.ToDecimal(employeeInformation[11]);
+            emp.SSN = employeeInformation[12];
+            emp.PhoneNumber = employeeInformation[13];
+            emp.County = employeeInformation[14];
+            emp.City = employeeInformation[15];
+            emp.State = employeeInformation[16];
+            emp.ZipCode = employeeInformation[17];
+            emp.UserName = employeeInformation[18];
+            emp.Password = employeeInformation[19];
+
+            employeeInfoData.Add(employeeInformation[0], emp);
+          }
+          i++;
+        }
+      }
+      //Array arr = employeeInfoData.Select(z => z.Value).ToArray();
+
+      string docPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+      using (StreamWriter outputCSVFile = new StreamWriter(Path.Combine(docPath, "WriteLines.csv")))
+      {
+        foreach (KeyValuePair<string, Employee> item in employeeInfoData)
+        {
+          string[] empValues = new string[]
+          {
+            item.Value.ID, item.Value.NamePrefix, item.Value.FirstName, item.Value.MiddleInitial
+          };
+          
+          outputCSVFile.WriteLine(string.Join(",", empValues));
+
+        }
+      }
+    }
+  }
+}
